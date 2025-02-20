52d4eb3e565685a4e4c1cef270945a9767cc51d7
nathanieluriri <uririnathaniel@gmail.com>
2025-02-20 03:18:34 +0100

    added new section for about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,4 +1,6 @@
 import { Card, CardContent } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+
 import {
   Speech,
   NotebookPen,
@@ -14,22 +16,95 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { Padding } from "../page";
+
+
+export function DropDownAbout() {
+  return (
+
+    <>
+    <div>
+      <p className="transition-all duration-100 text-lg font-bold px-1.5">About Me</p>
+      <h1 className="transition-all duration-100 bg-slate-100 text-black text-4xl font-medium me-2 px-1.5 py-1.5 rounded dark:bg-slate-900 dark:text-white w-fit">Evolving to create impactful experiences</h1>
+    <Accordion type="multiple" collapsible className="md:w-4/5 w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger className="px-1.5 transition-all duration-200">Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger className="px-1.5 transition-all duration-200">Is it styled?</AccordionTrigger>
+        <AccordionContent>
+        As a Product Designer, my background in Psychology and Education provides me with a unique perspective rooted in understanding human behavior and learning. This allows me to approach design from a user’s viewpoint like no other. My journey in Product Design has been enriched by my ability to empathize with users and discover their needs and desires through supported research. I firmly believe that effective design transcends mere aesthetics; it must also enhance the user experience, ensuring that products are not only visually appealing but also functional and intuitive. Through diverse work experiences, I’ve cultivated a distinctive blend of wisdom and professionalism, setting me apart from my peers. My commitment lies in harnessing these qualities to make a significant impact.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger className="px-1.5 transition-all duration-200">Is it animated?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It's animated by default, but you can disable it if you prefer.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+    </div>
+    </>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default function About() {
   return (
     <>
+    <AboutMeSection/>
+    <Padding/>
       <WhatCanIOfferSection />
     </>
   );
 }
 
+function AboutMeSection() {
+  return <div className="flex flex-col md:flex-row gap-10 w-4/5 justify-center justify-self-center transition-all duration-300">
+    <Skeleton className="w-full transition-all duration-1000 dark:bg-zinc-900 md:w-4/5 h-[300px] rounded" />
+    <Padding/>
+    <DropDownAbout />
+    <Padding/>
+  </div>;
+}
+
 function WhatCanIOfferSection() {
   return (
     <div className="flex lg:w-fit lg:max-w-[1000px]   justify-center justify-self-center flex-col">
+      <Padding/>
+      
       <h1 className="ml-7 sm:m-0 transition-all duration-100 bg-zinc-100 text-black text-xl font-medium me-2 px-1.5 py-1.5 rounded dark:bg-zinc-900 dark:text-white w-fit">
         WHAT I CAN OFFER
       </h1>
       <OfferCarousel />
+      <Padding/>
     </div>
   );
 }
