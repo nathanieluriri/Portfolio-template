2f862d22bc951d4a7e206f0fda5b07a71dd67e97
nathanieluriri <uririnathaniel@gmail.com>
2025-02-20 02:05:08 +0100

    added footer

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,76 @@
-
-import { Skeleton } from "@/components/ui/skeleton"
-
-
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Speech,
+  NotebookPen,
+  Eclipse,
+  AppWindow,
+  Brain,
+  MonitorSmartphone,
+} from "lucide-react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Padding } from "../page";
+const icons = [
+  <Speech className=" w-[60px] h-[60px]" />,
+  <NotebookPen className=" w-[60px] h-[60px]" />,
+  <Eclipse className=" w-[60px] h-[60px]" />,
+  <AppWindow className=" w-[60px] h-[60px]" />,
+  <Brain className=" w-[60px] h-[60px]" />,
+  <MonitorSmartphone className=" w-[60px] h-[60px]" />,
+];
+const titles = [
+  "Communication & Collaboration",
+  "User Research",
+  "Visual Design",
+  "Developer Friendly",
+  "AI Centered Applications",
+  "Mobile & Web Apps",
+];
+const contents =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.";
 export default function About() {
   return (
-    <h1>About Page</h1>
+    <>
+      <div className="flex w-fit max-w-[1000px]   justify-center justify-self-center flex-col">
+        <h1 className="transition-all duration-100 bg-zinc-100 text-black text-xl font-medium me-2 px-1.5 py-1.5 rounded dark:bg-zinc-900 dark:text-white w-fit">
+          WHAT I CAN OFFER
+        </h1>
+        <OfferCarousel />
+      </div>
+    </>
+  );
+}
+
+export function OfferCarousel() {
+  return (
+    <Carousel
+      opts={{
+        align: "start",
+      }}
+      className="w-full max-w-fit p-3 "
+    >
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className=" md:basis-2/6 basis-2/6 ">
+            <Card>
+              <CardContent className="pr-4 flex items-start justify-start flex-col pt-2 min-h-[300px] gap-2 ">
+                {icons[index]}
+                <h1 className=" text-3xl text-start">{titles[index]}</h1>
+                <h1 className="max-w-[250px]  text-start text-sm">
+                  {contents}
+                </h1>
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
   );
 }
