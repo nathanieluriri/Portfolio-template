6cd995dd585065fbbbd8801f225aa084e525af97
nathanieluriri <uririnathaniel@gmail.com>
2025-02-22 12:44:07 +0100

    hopefully fixing flash

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -26,7 +26,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" class="dark" style="color-scheme: dark;">
+    <html lang="en" >
       <body className=" dark:bg-black  transition-all duration-1000 grid">
         <ThemeProvider attribute="class" defaultTheme="dark">
           <Navbar />
