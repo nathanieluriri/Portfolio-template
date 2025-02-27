913639cb3a375c9bffde637ab66d3f0265d4daa5
nathanieluriri <uririnathaniel@gmail.com>
2025-02-27 13:13:06 +0100

    changed all work content

--- src/app/all-work/page.jsx
+++ src/app/all-work/page.jsx
@@ -2,7 +2,7 @@ import { TotalWorkSection } from "../page"
 export default function Page(){
     return(
        <>
-       <TotalWorkSection name={"All works"} description={"You will find all the projects Ive worked on here"}/>
+       <TotalWorkSection name={"All works"} description={"Here, you will find a curated collection of all the projects I have worked on. Browse through in-depth case studies, explore polished final designs, and interact with fully functional prototypes that showcase my expertise and problem-solving approach."}/>
 
        </>
         
